4ab44af6fed2a67b1e7bf22dc7532c6215f69c40
Reuzer <sergeyreunin1@gmail.com>
2024-02-06 15:25:26 +0300

    fff

--- lab1.php
+++ lab1.php
@@ -9,7 +9,7 @@
 <body>
     <header class="header">
         <div class="header__wrapper">
-            <img class="header__logo" src="Logo_Polytech_rus_main.jpg" alt="Polytech-logo">
+            <img class="header__logo" src="Logo_Polytech_rus_main.jpg" alt="Polytech-logo" width="200" height="100">
             <h1>Задание Hello-World</h1>
         </div>      
     </header>

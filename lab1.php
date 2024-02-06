121ad5395b6303e7da388bf065d26a7df1edf60c
Reuzer <sergeyreunin1@gmail.com>
2024-02-06 15:57:19 +0300

    lab1full

--- lab1.php
+++ lab1.php
@@ -9,9 +9,17 @@
 <body>
     <header class="header">
         <div class="header__wrapper">
-            <img class="header__logo" src="Logo_Polytech_rus_main.jpg" alt="Polytech-logo" width="200" height="100">
-            <h1>Задание Hello-World</h1>
+            <img class="header__logo" src="Logo_Polytech_rus_main.jpg" alt="Polytech-logo" width="350" height="100">
+            <h1 class="header__text">Задание Hello-World</h1>
         </div>      
     </header>
+    <main>
+        <?php
+            echo("Hello World!");
+        ?>
+    </main>
+    <footer>
+        <p class>Реунин Сергей Эдуардович</p>
+    </footer>
 </body>
 </html>
